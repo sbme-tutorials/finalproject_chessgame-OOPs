2240497828e88b610db2ffe218b1fdc7c4151b5f
Nouran-Hani <shouranshouran2000@gmail.com>
2023-04-27 23:42:42 +0200

    Frame class is added

---
+++ src/chessGame/Frame.java
@@ -0,0 +1,22 @@
+package chessGame;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+	
+	Frame(){
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("chessGame");
+		this.setResizable(false);
+		this.setSize(800, 550);
+		//this.setDefaultLookAndFeelDecorated(true);
+
+		
+		ImageIcon image = new ImageIcon("logo.jpg");
+		this.setIconImage(image.getImage());
+		
+	}
+
+}
---
+++ src/chessGame/Main.java
@@ -0,0 +1,10 @@
+package chessGame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Frame frame = new Frame();
+
+	}
+
+}
