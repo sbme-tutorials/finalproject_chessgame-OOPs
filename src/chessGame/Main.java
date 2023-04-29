e3ec8b447fa76ad76ace75d39dce530ec15d471a
Nouran-Hani <shouranshouran2000@gmail.com>
2023-04-29 14:28:45 +0300

    changrs due to timer test

--- src/chessGame/Main.java
+++ src/chessGame/Main.java
@@ -2,24 +2,25 @@ package chessGame;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.event.ActionListener;
 
 import javax.swing.JLabel;
+import javax.swing.Timer;
 
 public class Main {
 
 	public static void main(String[] args){
+		
+		//Timer timer = new Timer(1000, ActionListener());
+		
 		Frame frame = new Frame();
 		Panel panel1 = new Panel();
 		Panel panel2 = new Panel();
-		JLabel label1 = new JLabel();
-		JLabel label2 = new JLabel();
+		Label label1 = new Label();
+		Label label2 = new Label();
 		
 		label1.setText("00:00");
 		label2.setText("00:00");
-		label1.setFont(new Font("Monospaced", Font.PLAIN, 22));
-		label1.setForeground(Color.WHITE);
-		label2.setFont(new Font("Monospaced", Font.PLAIN, 22));
-		label2.setForeground(Color.WHITE);
 		
 		
 		panel1.setBounds(600, 50, 100, 50);
@@ -29,7 +30,24 @@ public class Main {
 		frame.add(panel2);
 		panel1.add(label1);
 		panel2.add(label2);
-
-	}
+		
+		/*
+		 * timer.start(); }
+		 * 
+		 * private static ActionListener ActionListener() { labela.setText(timePassed())
+		 * return null; }
+		 * 
+		 * private static Object timePassed() { int counterm = 10; int counters = 10;
+		 * 
+		 * @Override public void run() { if (counterm>=0&&counters>=0) {
+		 * System.out.printf("%02d:%02d\n",counterm, counters); counters--;
+		 * if(counters==0&&counterm!=0) { counterm--; counters = 60; } } else {
+		 * System.out.println("Time is up"); timer.cancel(); }
+		 * 
+		 * }
+		 * 
+		 * timer.scheduleAtFixedRate(task, 0, 1000); return null;
+		 */	}
 
 }
+
--- src/chessGame/Panel.java
+++ src/chessGame/Panel.java
@@ -1,7 +1,9 @@
 package chessGame;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Font;
 
 public class Panel extends JPanel{
 	{
@@ -10,5 +12,15 @@ public class Panel extends JPanel{
 	
 	
 
+	}
+}
+
+class Label extends JLabel{
+	{
+		this.setFont(new Font("Arial", Font.PLAIN, 38));
+		this.setHorizontalAlignment(JLabel.CENTER);
+		this.setVerticalAlignment(JLabel.CENTER);
+		this.setForeground(Color.WHITE);
+		
 	}
 }
