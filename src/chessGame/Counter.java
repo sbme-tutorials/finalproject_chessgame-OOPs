2489c8016617843394731bf3561e25baae3cb45e
Nouran-Hani <shouranshouran2000@gmail.com>
2023-04-29 14:27:54 +0300

    timer test

---
+++ src/chessGame/Counter.java
@@ -0,0 +1,21 @@
+/*
+ * package chessGame;
+ * 
+ * import java.awt.event.ActionEvent; import java.awt.event.ActionListener;
+ * 
+ * import javax.swing.Timer;
+ * 
+ * public class Counter extends Timer{ int seconds; Label label = new Label();
+ * Frame frame = new Frame(); Panel panel = new Panel();
+ * 
+ * public Counter(int delay, ActionListener listener) { super(delay, listener);
+ * seconds++; label.setText(""+ seconds);
+ * 
+ * 
+ * }
+ * 
+ * 
+ * 
+ * 
+ * }
+ */
