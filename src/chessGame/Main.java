b157b6d65f6263168877bedc88a50225adcac237
Nouran-Hani <shouranshouran2000@gmail.com>
2023-04-30 16:15:22 +0300

    updating login frame

--- src/chessGame/Main.java
+++ src/chessGame/Main.java
@@ -4,7 +4,10 @@ public class Main {
 
 	public static void main(String[] args){
 		
-		new GameView();
+		//new GameView();
+		IDandPassword idandpassword = new IDandPassword();
+
+        LoginPage loginpage  = new LoginPage(idandpassword.getLoginInfo());
 
 		
 	}
