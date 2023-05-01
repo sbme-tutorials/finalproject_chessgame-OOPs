b2d50d96e7f14a6207070d19ef2264811e6d3feb
Nouran-Hani <shouranshouran2000@gmail.com>
2023-05-01 15:54:21 +0300

    adding history frame

--- src/chessGame/Main.java
+++ src/chessGame/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
 	public static void main(String[] args){
 
-		new GameView().main(args);
+		//new GameView().main(args);
 		
 		//IDandPassword idandpassword = new IDandPassword();
 
         //new LoginPage(idandpassword.getLoginInfo());
-
+		
+		new History("nour", "eman", 1, "02:00");
 		
 	}
 }
