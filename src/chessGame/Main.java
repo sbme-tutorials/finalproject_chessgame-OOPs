2e43720deed51ace618afb4b19941f6dce1ffeed
Nouran-Hani <shouranshouran2000@gmail.com>
2023-05-03 05:28:59 +0300

    مش فاكرة غيرت ايه

--- src/chessGame/Main.java
+++ src/chessGame/Main.java
@@ -1,12 +1,22 @@
 package chessGame;
 
+import log_in.Choose;
+
 public class Main {
 
 	public static void main(String[] args){
 
-		//new GameView().main(args);
-
-
+		//new GameView();
+		
+        //new LoginPage(new IDandPassword().getLoginInfo());
+		
+		//new History("nour", "eman", 1, "02:00");
+		
+		//new historyOrPlayNow();
+		
+		//new welcomePage();
+		
+		//new Choose().Lastframe();
 	}
-
 }
+
